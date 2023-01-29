4ff326b9104f9c08bda3767dbd4ccac5338c3986
Ugochukwu Onah <113071405+Fabulousugo@users.noreply.github.com>
2023-01-29 21:39:07 +0100

    Update [id].tsx

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -12,6 +12,16 @@ type Props = {
   publication: PublicationQuery;
 };
 
+
+/*
+This is a Next.js component for rendering a single post. It exports 3 functions: the default component, getStaticProps, and getStaticPaths.
+
+The default component PostPage takes a publication prop and renders it using the PostView component.
+getStaticProps is used for server-side rendering and it asynchronously fetches data for the post using the fetchData function and a GraphQL query PublicationDocument.
+It returns an object with a props property, which is the data for the publication prop.
+getStaticPaths is used to generate the list of all possible paths for this page. Currently,
+it returns an empty paths array and sets the fallback value to "blocking". It looks like this function needs to be implemented to fetch all post IDs.
+*/
 export default function PostPage({ publication }: Props) {
   return <PostView publication={publication} />;
 }
